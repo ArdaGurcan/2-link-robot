4f3913b86e2574348dda54992df22896d675f226
ArdaGurcan <ardagurcan2004@gmail.com>
2021-08-17 16:03:35 +0300

    add method for generating configuration field

--- main.js
+++ main.js
@@ -24,14 +24,43 @@ function draw() {
         ellipse(obstacles[i][0], obstacles[i][1], obstacles[i][2])
         
     }
-    
+    for (let i = 0; i < points.length; i++) {
+        for (let j = 0; j < points[i].length; j++) {
+            if(points[i][j])
+            {
+                rect(height + margin + i *5/resolution / 180*boxSize, margin + j*10/resolution/ 360*boxSize,boxSize/36/resolution)
+                
+            }
+        }
+    }
     // drawArms()
     // fill("#911")
     // ellipse(10 + 200 +  s1Length * sin(a) + s2Length * sin(b+a), 390 - s1Length * cos(a) - s2Length * cos(b+a), 10)
     // fill("#fff")
 }
 
+function generateCollision() {
+    clear()
+    rect(margin, margin, boxSize, boxSize);
+    rect(height + margin, margin, boxSize, boxSize);
+    for (a = -90; a < 90; a+=5/resolution) {
+        let row = []
+        for (b = -180; b < 180; b+=10/resolution) {
+            if ((boxSize)/2 > s1Length * sin(a) + s2Length * sin(b+a) && s1Length * sin(a) + s2Length * sin(b+a) > -(boxSize)/2&& 
+            s1Length * cos(a) + s2Length * cos(b+a) > 0 
+            ) {
+                    row.push(true)
+            }
+            else
+            {
+                row.push(false)
+            }
 
+        }
+        points.push(row)
+    }
+    
+}
 
 function drawArms() {
     push();
